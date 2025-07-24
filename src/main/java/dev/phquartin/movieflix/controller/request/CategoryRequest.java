6ea43f639851ec253f50d9d5990e90ed64c38cff
phquartin <phquartinmatos@sempreceub.com>
2025-07-23 23:53:07 -0300

    fix: adicionar validação de null no campo NAME em CategoryRequest
    - Incluir anotação `@NotNull` para validar que o campo NAME não seja nulo

--- src/main/java/dev/phquartin/movieflix/controller/request/CategoryRequest.java
+++ src/main/java/dev/phquartin/movieflix/controller/request/CategoryRequest.java
@@ -1,10 +1,12 @@
 package dev.phquartin.movieflix.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CategoryRequest(
         @NotBlank(message = "Category NAME cannot be blank")
+        @NotNull(message = "Category NAME cannot be null")
         String name
 ) {}
