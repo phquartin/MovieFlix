193d09909a852564658881d250d9b2eadbaa43c6
phquartin <phquartinmatos@sempreceub.com>
2025-07-24 00:17:48 -0300

    refactor: adicionar anotação @Builder em CategoryRequest e CategoryResponse
    - Reintroduzir a anotação `@Builder` nas classes `CategoryRequest` e `CategoryResponse` para suportar construção de objetos com padrão builder

--- src/main/java/dev/phquartin/movieflix/controller/request/CategoryRequest.java
+++ src/main/java/dev/phquartin/movieflix/controller/request/CategoryRequest.java
@@ -2,7 +2,9 @@ package dev.phquartin.movieflix.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record CategoryRequest(
         @NotBlank(message = "Category NAME cannot be blank")
         @NotNull(message = "Category NAME cannot be null")
--- src/main/java/dev/phquartin/movieflix/controller/response/CategoryResponse.java
+++ src/main/java/dev/phquartin/movieflix/controller/response/CategoryResponse.java
@@ -1,5 +1,7 @@
 package dev.phquartin.movieflix.controller.response;
 
+import lombok.Builder;
 
+@Builder
 public record CategoryResponse(Long id, String name) {
 }
