a7f9247670f134c58c524dfe4815002e8b029060
phquartin <phquartinmatos@sempreceub.com>
2025-07-24 00:06:52 -0300

    refactor: remover anotação @Builder de CategoryRequest e CategoryResponse
    - Excluir a anotação `@Builder` das classes `CategoryRequest` e `CategoryResponse` por não ser necessária

--- src/main/java/dev/phquartin/movieflix/controller/request/CategoryRequest.java
+++ src/main/java/dev/phquartin/movieflix/controller/request/CategoryRequest.java
@@ -2,9 +2,7 @@ package dev.phquartin.movieflix.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 
-@Builder
 public record CategoryRequest(
         @NotBlank(message = "Category NAME cannot be blank")
         @NotNull(message = "Category NAME cannot be null")
--- src/main/java/dev/phquartin/movieflix/controller/response/CategoryResponse.java
+++ src/main/java/dev/phquartin/movieflix/controller/response/CategoryResponse.java
@@ -1,7 +1,5 @@
 package dev.phquartin.movieflix.controller.response;
 
-import lombok.Builder;
 
-@Builder
 public record CategoryResponse(Long id, String name) {
 }
