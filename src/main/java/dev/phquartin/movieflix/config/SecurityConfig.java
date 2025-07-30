7d51ae3a56784d1ba6691f97017c6452fa5a8ffd
phquartin <phquartinmatos@sempreceub.com>
2025-07-29 23:57:59 -0300

    feat: permitir acesso público à documentação da API
    - Adicionar permissões públicas para endpoints `/swagger-ui/**` e `/v3/api-docs/**` em `SecurityConfig`

--- src/main/java/dev/phquartin/movieflix/config/SecurityConfig.java
+++ src/main/java/dev/phquartin/movieflix/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
