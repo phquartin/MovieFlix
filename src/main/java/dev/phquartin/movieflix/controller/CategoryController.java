f69500b4b05a9021771c56a255306218f5707235
phquartin <phquartinmatos@sempreceub.com>
2025-07-29 22:02:37 -0300

    refactor: remover importações desnecessárias nas controladoras
    - Limpar importações não utilizadas após refatoração

--- src/main/java/dev/phquartin/movieflix/controller/CategoryController.java
+++ src/main/java/dev/phquartin/movieflix/controller/CategoryController.java
@@ -8,9 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
